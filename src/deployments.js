f5d5e086bcc490682857551b8b8062502426e9e3
Will Voelcker <wvoelcker@yahoo.co.uk>
2019-01-28 11:27:32 +0000

    Update deploy script to support multiple actions

--- src/deployments.js
+++ src/deployments.js
@@ -5,6 +5,7 @@ const cli = require('commander')
 cli
 	.version("0.1.0")
 	.option("-t --topics <topics-file>", "The path to the YAML file containing the list of topics")
+	.option("-a --action <action-name>", "The action to perform on the specified deployment (e.g. create, update)")
 	.option("-d --deployment <deployment-name>", "The name of the deployment to manage")
 	.parse(process.argv)
 
@@ -16,9 +17,14 @@ if (!topicsPath.test(rgxSafeForShell)) {
 	throw new Error("Unsafe topics-file path")
 }
 
+const actionName = cli.action
+if (!actionName.test(rgxSafeForShell)) {
+	throw new Error("Unsafe action name")
+}
+
 const deploymentName = cli.deployment
 if (!deploymentName.test(rgxSafeForShell)) {
 	throw new Error("Unsafe deployment name")
 }
 
-shell.exec(`cp '${topicsPath}' '${__dirname}' && gcloud deployment-manager deployments '${deploymentName}' --config '${__dirname}/deploy.yaml'`)
+shell.exec(`cp '${topicsPath}' '${__dirname}' && gcloud deployment-manager deployments '${actionName}' ${deploymentName}' --config '${__dirname}/deploy.yaml'`)
