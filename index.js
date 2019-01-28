be4a22caf86179a3f0565fe9de84ed46dd0ba64c
Will Voelcker <wvoelcker@yahoo.co.uk>
2019-01-28 12:47:02 +0000

    Removing obsolete code

--- index.js
+++ index.js
@@ -2,12 +2,6 @@
 const path = require("path")
 const yaml = require("./src/yaml")
 
-if (!module.parent) {
-	throw new Error("No parent module")
-}
-if (!module.parent.filename) {
-	throw new Error("No parent module filename could be determined")
-}
 
 exports.loadFrom = filepath => {
 	const doc = yaml.loadFileSync(topicsFile)
